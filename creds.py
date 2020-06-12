44c8a820bfca73b4777d7c622d54a30c3a1f625f
Pranjal Jain <24854511+yellowjacketcoder@users.noreply.github.com>
2020-06-12 14:00:26 +0530

    Added my credentials
    Added Untitled Folder in Jain Drive to Credentials

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1228976874:AAEiT_WFWVe-UOKL6rXdaqzxfIh25NnLZ-8"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "18mzevlC_mC7eig1V9VMlLAhPejEutJBX"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AOWQwq5vunAdUk9PVA"
     
     
     
